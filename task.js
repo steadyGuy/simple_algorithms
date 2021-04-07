04f72c0e78c9f30dbd6a17a66dc827978fe26366
Yuriy Soproniuk <sopronukCC@gmail.com>
2021-04-07 21:26:17 +0300

    some updates

---
+++ task.js
@@ -0,0 +1,55 @@
+"use strict";
+const count100 = "word fsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348hwer34r";
+
+const count5000 = "word fsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdfsjihfsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd2343-2=455-24=-1-22\e[wekrpwejfmklcdnvffdgdfg/df*g/d*/g*/df*gfgddffgsdufh8h348h438hfhd2h23foh294gh92j9j9j9huhsdnfs.dfsdfs'gfdgdfdfggfsddfsd234";
+
+let countOverflow = "hello ";
+for (let i = countOverflow.length; i < 22000; i++) {
+  countOverflow += Math.floor(Math.random() * 9);
+}
+
+function reverseOderRec(str, n) {
+  n = typeof n === 'undefined' ? str.length - 1 : n - 1;
+  return n >= 0 ? str[n] + reverseOderRec(str, n) : '';
+}
+
+function directOrederRec(str, n = 0) {
+  return (n != str.length) ? str[n] + directOrederRec(str, ++n) : '';
+}
+
+function reverseOderIter(str) {
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+}
+
+function directOderIter(str) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    newStr += str[i];
+  }
+  return newStr;
+}
+
+function bench(data, callback) {
+  let start = new Date();
+  callback(data);
+  console.log(`${callback.name} - ${new Date() - start} милисекунд`)
+}
+
+bench(count100.split(" ")[1], reverseOderRec);
+bench(count100.split(" ")[1], directOrederRec);
+bench(count100.split(" ")[1], reverseOderIter);
+bench(count100.split(" ")[1], directOderIter);
+console.log();
+bench(count5000.split(" ")[1], reverseOderRec);
+bench(count5000.split(" ")[1], directOrederRec);
+bench(count5000.split(" ")[1], reverseOderIter);
+bench(count5000.split(" ")[1], directOderIter);
+console.log();
+bench(countOverflow.split(" ")[1], reverseOderRec);
+bench(countOverflow.split(" ")[1], directOrederRec);
+bench(countOverflow.split(" ")[1], reverseOderIter);
+bench(countOverflow.split(" ")[1], directOderIter);
